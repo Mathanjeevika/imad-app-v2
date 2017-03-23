782d7c537bc42d5cad1862ed3594ac7d97810433
Mathanjeevika <Mathanjeevika@users.noreply.github.com>
2017-03-23 19:58:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,16 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
-
-var config = {
-    user:'mathanjeevika',
-    database:'mathanjeevika',
-    host:'db.imad.hasura.app-io',
-    port:'5432',
-    password:process.emv.DB.PASSWORD
-}
-
 var app = express();
 app.use(morgan('combined'));
 
