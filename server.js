54669a75911f9ed58dc5493b53edef47c938e7ee
Mathanjeevika <Mathanjeevika@users.noreply.github.com>
2017-03-24 08:03:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,9 +6,27 @@ var app =express();
 app.use(morgan('combined'));
 
 
+
+app.get('/article-one',function(req,res) {
+    res.sendFile(path,join(_dirname,'ui','article-one.html'));
+});
+
+app.get('/article-two' ,function(req,res) {
+    res.sendFile(path,join(_dirname,'ui','article-two.html'));
+});
+
+app.get('/article-four',function(req,res) {
+    res.sendFile(path,join(_dirname,'ui','article-four.html'));
+});
+
+app.get('/article-three',function(req,res) {
+    res.sendFile(path,join(_dirname,'ui','article-three.html'));
+});
 app.get('/',function(req,res) {
     res.sendFile(path,join(_dirname,'ui','index.html'));
 });
+
+
 
 app.get('/ui/styleclass',function(req,res) {
     res.sendFile(path,join(_dirname,'ui','styleclass.css'));
