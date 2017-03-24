115674922e17f1a2e981176167a97f430335271a
Mathanjeevika <Mathanjeevika@users.noreply.github.com>
2017-03-24 13:25:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,9 +106,27 @@ function createTemplate(data){
         `;
         return htmlTemplate;
 }
-app.get('/:articleName', function (req, res) {
-    var articleName=req.parans.articleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function (req, res) {
+    pool.query("SELECT * FROM article WHERE title="+req.parans.articleName,function(err,result)
+    {
+       if(err)
+       {
+           res.status(500).send(err,toString());
+       }
+       else
+       {
+           if(result.row.length === 0)
+           {
+               res.status(404).send("ArticleNot Found");
+           }
+           else
+           {
+               var articleData = result.rows[0];
+               res.send(createTemplate(articleData));
+           }
+       }
+    });
+
 });
 
 app.get('/ui/style.css', function (req, res) {
