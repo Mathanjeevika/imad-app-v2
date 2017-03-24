04affb706798b6c867bf2b3805ec3f447b02b538
Mathanjeevika <Mathanjeevika@users.noreply.github.com>
2017-03-24 11:42:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 
 var element=document.getElementById("main-text");
 element.innerHTML ="new value";
+
+var img =document.getElementById('modi');
+img.onclick=function(){
+   image.style.marginRight ='100px';
+};
